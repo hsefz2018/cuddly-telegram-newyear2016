197ae25fcd51de6c8e4da9a72ceea450a0b457f1
Pisces000221 <1786762946@qq.com>
2015-12-29 00:35:16 +0800

    Simplify manual testing

--- monitor.js
+++ monitor.js
@@ -18,7 +18,16 @@
     // console.log(e.clientX + ' ' + e.clientY);
     commenting.highlight_comment(e.clientX, e.clientY);
   };
-  document.getElementById('comment-canvas').onmousedown = function (e) {
+  document.getElementById('comment-canvas').onmouseup = function (e) {
     commenting.remove_comment();
   };
+  var random_colour = function () {
+    var colour_code = Math.floor(Math.random() * 256 * 256 * 256);
+    var s = colour_code.toString(16);
+    while (s.length < 6) s = '0' + s;
+    return '#' + s;
+  };
+  setInterval(function () {
+    window.commenting.fire({id: Math.floor(Math.random() * 0xffffff), message: 'xxxxxx', position: 0, color: random_colour()})
+  }, 300);
 }(window));
