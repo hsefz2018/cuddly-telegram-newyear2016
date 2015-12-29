049b1abc861ef1557e7c545977be67dbce337eae
Pisces000221 <1786762946@qq.com>
2015-12-29 19:54:22 +0800

    Automatically reconnect

--- main-doubled.js
+++ main-doubled.js
@@ -15,20 +15,26 @@
   };
 
   //var socket = new WebSocket('<?php $channel = new SaeChannel();echo $channel -> createChannel('danmaku',18000);?>');
-  var xhr = new XMLHttpRequest();
-  xhr.open('GET', 'http://app.efzer.org/live/2016newyear/danmaku.php', false);
-  xhr.send();
-  var socket = new WebSocket(xhr.response.substr(xhr.response.indexOf('ws://'), 115));
-  window.last_received_id = -1;
-  socket.onmessage = function (msg) {
-    var c = JSON.parse(msg.data);
-    if (c.id == window.last_received_id) return;
-    window.last_received_id = c.id;
-    if (c.color == 'blue' || c.color == '#00f' || c.color == '#0000ff') c.color = '#66f';
-    c.position = comment_type_names[c.position];
-    c.message = html_decode(c.message);
-    window.commenting.fire(c);
-    setTimeout((function (_c) { return function () { window.commenting.fire(_c); }; })(c), 15000);
+  window.c_socket = null;
+  var reconnect = function () {
+    if (window.c_socket) window.c_socket.close();
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', 'http://app.efzer.org/live/2016newyear/danmaku.php', false);
+    xhr.send();
+    window.c_socket = new WebSocket(xhr.response.substr(xhr.response.indexOf('ws://'), 115));
+    window.last_received_id = -1;
+    window.c_socket.onmessage = function (msg) {
+      var c = JSON.parse(msg.data);
+      if (c.id == window.last_received_id) return;
+      window.last_received_id = c.id;
+      if (c.color == 'blue' || c.color == '#00f' || c.color == '#0000ff') c.color = '#66f';
+      c.position = comment_type_names[c.position];
+      c.message = html_decode(c.message);
+      window.commenting.fire(c);
+      setTimeout((function (_c) { return function () { window.commenting.fire(_c); }; })(c), 15000);
+    };
+    // window.commenting.fire({id: 1, message: 'xxx', position: 0, color: 'red'})
   };
-  // window.commenting.fire({id: 1, message: 'xxx', position: 0, color: 'red'})
+  reconnect();
+  setInterval(reconnect, 300000);
 }(window));
