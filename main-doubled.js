e30049857354f7f1bcb3c8ab29c2f6433bb8c250
Pisces000221 <1786762946@qq.com>
2015-12-29 19:14:54 +0800

    Prevent duplicate comments

--- main-doubled.js
+++ main-doubled.js
@@ -19,8 +19,11 @@
   xhr.open('GET', 'http://app.efzer.org/live/2016newyear/danmaku.php', false);
   xhr.send();
   var socket = new WebSocket(xhr.response.substr(xhr.response.indexOf('ws://'), 115));
+  window.last_received_id = -1;
   socket.onmessage = function (msg) {
     var c = JSON.parse(msg.data);
+    if (c.id == window.last_received_id) return;
+    window.last_received_id = c.id;
     if (c.color == 'blue' || c.color == '#00f' || c.color == '#0000ff') c.color = '#66f';
     c.position = comment_type_names[c.position];
     c.message = html_decode(c.message);
